81e22aa98fb45aecb0033213bd70f4cb07e00614
Kittiphun Fuankkorn <57912713+Kittiphun-Arm@users.noreply.github.com>
2020-10-19 12:41:24 +0700

    Delete teat3.py

--- python/teat3.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient("mongodb+srv://admin:68276728@projectdisasterrisk.wh8co.gcp.mongodb.net/DATAPROJECT?retryWrites=true&w=majority")
-db = client.test
-col = db["testcollection"]
-col.insert_one({"data1":"1","data2":"2"})
